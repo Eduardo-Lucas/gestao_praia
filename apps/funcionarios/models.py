237beb7cd06ace80ba609aee4a9c51d7f73ded7c
Eduardo-Lucas-Diletta <eduardo.lucas@dilettasolutions.com>
2022-04-27 14:16:37 -0300

    Criando a app Departamento

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+from apps.departamentos.models import Departamento
 
 
 class Funcionario(models.Model):
+    user = models.ForeignKey(User, default=1, on_delete=models.PROTECT)
+    departamento = models.ForeignKey(Departamento, default=1, on_delete=models.PROTECT)
     nome = models.CharField(max_length=100, help_text="Nome do Funcionário")
 
     def __str__(self):
