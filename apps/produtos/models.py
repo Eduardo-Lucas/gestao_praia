f8c4230ba3e8671a9baadaedf007b985507489a3
Eduardo-Lucas-Diletta <eduardo.lucas@dilettasolutions.com>
2022-04-28 10:43:39 -0300

    Adicionado o campo slug na class Produto

--- apps/produtos/models.py
+++ apps/produtos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 from apps.empresas.models import Empresa
 
@@ -19,6 +20,7 @@ class Produto(models.Model):
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
     nome = models.CharField(max_length=50, help_text='Nome do Produto')
+    slug = models.SlugField(max_length=200, db_index=True, default=' ')
     image = models.ImageField(upload_to='produtos', blank=True, null=True)
     disponivel = models.BooleanField(default=True, help_text='Tem produto no estoque?')
     preco = models.DecimalField(max_digits=10, decimal_places=2, help_text='Preço do Produto')
@@ -29,3 +31,7 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.nome
+
+    def save(self):
+        self.slug = slugify(self.descricao)
+        super(Produto, self).save()
